698d653771445dea94f09db602c14fe6937b5a5d
David Hale and Javier Jimenez <david@>
2017-08-08 16:33:02 -0700

    add better js

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,10 @@
 $(document).ready(function() {
-  $(".clickableJS").click(function() {
-    $(".definitionJS").toggle(800);
-    $(".termJS").toggle(800);
-    $("#JS").toggleClass('color');
+  $(document).ready(function() {
+    $('.flashCard').click(function(e) {
+      var kids = $(e.currentTarget).children();
+      //var x = $(e.currentTarget);
+      kids.toggle(800);
+      //x.toggleClass('.color');
+    });
   });
-
-  $(".clickableOP").click(function() {
-    $(".definitionOP").toggle(800);
-    $(".termOP").toggle(800);
-    $("#OP").toggleClass('color');
-  });
-
-  $(".clickableVA").click(function() {
-    $(".definitionVA").toggle(800);
-    $(".termVA").toggle(800);
-    $("#VA").toggleClass('color');
-  });
-
-  $(".clickableVN").click(function() {
-    $(".definitionVN").toggle(800);
-    $(".termVN").toggle(800);
-    $("#VN").toggleClass('color');
-  });
-
-  $(".clickableFU").click(function() {
-    $(".definitionFU").toggle(800);
-    $(".termFU").toggle(800);
-    $("#FU").toggleClass('color');
-  });
-
-  $(".clickableMT").click(function() {
-    $(".definitionMT").toggle(800);
-    $(".termMT").toggle(800);
-    $("#MT").toggleClass('color');
-  });
-
-  $(".clickableAR").click(function() {
-    $(".definitionAR").toggle(800);
-    $(".termAR").toggle(800);
-    $("#AR").toggleClass('color');
-  });
-
-  $(".clickablePR").click(function() {
-    $(".definitionPR").toggle(800);
-    $(".termPR").toggle(800);
-    $("#PR").toggleClass('color');
-  });
-
-  $(".clickableRE").click(function() {
-    $(".definitionRE").toggle(800);
-    $(".termRE").toggle(800);
-    $("#RE").toggleClass('color');
-  });
-
-  $(".clickableCM").click(function() {
-    $(".definitionCM").toggle(800);
-    $(".termCM").toggle(800);
-    $("#CM").toggleClass('color');
-  });
-
-  $(".clickableST").click(function() {
-    $(".definitionST").toggle(800);
-    $(".termST").toggle(800);
-    $("#ST").toggleClass('color');
-  });
-
-  $(".clickableBO").click(function() {
-    $(".definitionBO").toggle(800);
-    $(".termBO").toggle(800);
-    $("#BO").toggleClass('color');
-  });
-
 });
