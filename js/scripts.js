6c31b809cd7e3987052192f972d2743b14e44d1c
David Hale and Javier Jimenez <david@>
2017-08-08 16:40:19 -0700

    add color-change to well

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
-  $(document).ready(function() {
-    $('.flashCard').click(function(e) {
-      var kids = $(e.currentTarget).children();
-      //var x = $(e.currentTarget);
-      kids.toggle(800);
-      //x.toggleClass('.color');
-    });
+  $('.flashCard').click(function(e) {
+    var kids = $(e.currentTarget).children();
+    var x = $(e.currentTarget).toggleClass('color');
+    kids.toggle(800);
   });
 });
