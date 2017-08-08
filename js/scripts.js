44003cc92dfdc204e5ae36c4ad6e1f369b6a3a9b
David Hale and Javier Jimenez <david@>
2017-08-08 14:07:45 -0700

    add more definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,42 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".definition").toggle(800);
-    $(".term").toggle(800);
+  $(".clickableJS").click(function() {
+    $(".definitionJS").toggle(800);
+    $(".termJS").toggle(800);
   });
+
+  $(".clickableOP").click(function() {
+    $(".definitionOP").toggle(800);
+    $(".termOP").toggle(800);
+  });
+
+  $(".clickableVA").click(function() {
+    $(".definitionVA").toggle(800);
+    $(".termVA").toggle(800);
+  });
+
+  $(".clickableVN").click(function() {
+    $(".definitionVN").toggle(800);
+    $(".termVN").toggle(800);
+  });
+
+  $(".clickableFU").click(function() {
+    $(".definitionFU").toggle(800);
+    $(".termFU").toggle(800);
+  });
+
+  $(".clickableMT").click(function() {
+    $(".definitionMT").toggle(800);
+    $(".termMT").toggle(800);
+  });
+
+  $(".clickableAR").click(function() {
+    $(".definitionAR").toggle(800);
+    $(".termAR").toggle(800);
+  });
+
+  $(".clickablePR").click(function() {
+    $(".definitionPR").toggle(800);
+    $(".termPR").toggle(800);
+  });
+
 });
